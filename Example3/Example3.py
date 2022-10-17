37569cfe5aef685f9177041b424faaa3cb9f04c0
YankovichKirill <kirill.best97@gmail.com>
2022-10-17 19:26:21 +0300

    Example3

---
+++ Example3/Example3.py
@@ -0,0 +1,3 @@
+str1 = input("Enter the first line to test: ")
+str2 = input("Enter the second line to search in the first line: ")
+print(f"The second line is included in the first {str1.count(str2)} times(es).")
